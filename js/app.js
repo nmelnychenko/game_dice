b016f0afb6ac0b71b69bee1ff5dc7313251e0b3f
nmelnychenko <mykola.melnichenko@gmail.com>
2019-06-18 11:58:31 +0300

    refactor changePlayer

--- js/app.js
+++ js/app.js
@@ -1,15 +1,13 @@
 /*
 GAME RULES:
-
 - The game has 2 players, playing in rounds
 - In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
 - BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
 - The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
-
 */
 
-const RESET_VALUE = 1;
+const RESET_VALUE = 2;
 
 let scores = [0, 0];
 let activePlayer = 0;
@@ -27,26 +25,22 @@ const initGame = () => {
 initGame();
 
 document.querySelector('.btn-roll').addEventListener('click', function() {
+  let values = [];
 
-  diceElements.forEach(item => {
+  diceElements.forEach((item, index) => {
     let dice = Math.floor(Math.random() * 6) + 1;
 
     item.src = `img/dice-${dice}.png`;
     item.style.display = 'block';
+    values[index] = dice;
   })
 
-  // TODO: Should be refactored
-  // if (dice !== RESET_VALUE) {
-  //   current += dice;
-  //   document.getElementById('current-'+activePlayer).textContent = current;
-
-  //   if (scores[activePlayer] + current >= 20) {
-  //     alert(`Player ${activePlayer} won!!!`);
-  //   }
-    
-  // } else {
-  //   changePlayer();
-  // }
+  if (!values.includes(RESET_VALUE) && values[0] !== values[1]) {
+    current += values[0] + values[1];
+    document.getElementById('current-'+activePlayer).textContent = current;
+  } else {
+    changePlayer();
+  }
 });
 
 const changePlayer = () => {
@@ -54,7 +48,7 @@ const changePlayer = () => {
   document.getElementById('current-'+activePlayer).textContent = 0;
   document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
   activePlayer = +!activePlayer;
-  diceElement.style.display = 'none';
+  diceElements.forEach(item => item.style.display = 'none');
   document.querySelector(`.player-${activePlayer}-panel`).classList.toggle('active');
 }
 
